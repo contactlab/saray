bcba601fec9da2afca1f13fbdc6dbdb6e3bcd94c
Paolo Ferretti <paolo.ferretti@contactlab.com>
2016-08-25 11:25:44 +0200

    HTTP CORS fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
