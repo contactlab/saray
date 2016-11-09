fd604b51546203bad3d69982e7a46b81bf5cc708
Paolo Ferretti <paolo@paoloferretti.net>
2016-11-09 12:52:29 +0100

    Refs #5 - Custom API route response with tests

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 #! /usr/bin/env node
 
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
+const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
-const bodyParser = require('body-parser');
 const program = require('commander');
+
+const app = express();
 
 const allowedMethods = ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'];
 const DEFAULT_PORT = 8081;
@@ -16,6 +18,8 @@ program
   .description("Yet Another Rest API Stubber'.split(' ').reverse().map(item => item[0].toLowerCase()).join('')")
   .option('--port <port>', 'The port to listen to', DEFAULT_PORT)
   .option('--path <password>', 'The path for stubbed data', DEFAULT_PATH)
+  .option('--endpoint <endpoint>', 'The endpoint', null)
+  .option('--epfirst', 'Use only if you want to yield to endpoint', false)
   .parse(process.argv);
 
 app.use(bodyParser.json());
@@ -24,28 +28,15 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(function(req, res, next) {
-  // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', allowedMethods.join(', '));
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
   next();
 });
-
-const port = program.port;
-module.exports.port = port;
-
-const apiDataPath = program.path;
-module.exports.apiDataPath = apiDataPath;
 
 /**
  * Parse an HTTP parameters object and convert it into a query string
@@ -63,7 +54,7 @@ const getParamsString = function(rawParams) {
 };
 module.exports.getParamsString = getParamsString;
 
-app.all('/*', function(req, res) {
+function getQueryString(req) {
   let rawParams = {};
 
   // GET and POST parameters object are the same, but they are in different
@@ -76,16 +67,67 @@ app.all('/*', function(req, res) {
 
   const paramString = getParamsString(rawParams);
   const params = paramString !== '' ? '?' + paramString : '';
+  return params;
+}
+module.exports.getQueryString = getQueryString;
+
+function reallyAllowedMethods(req, params) {
+  return allowedMethods.filter(function(method) {
+    const filePath = path.join(module.exports.apiDataPath, req.path + params + '.' + method + '.json');
+    if(fs.existsSync(filePath)) {
+      return method;
+    }
+  });
+}
+module.exports.reallyAllowedMethods = reallyAllowedMethods;
+
+app.use(function(req, res, next) {
+  const endpoint = program.endpoint;
+
+  if (endpoint !== null) {
+    const params = getQueryString(req);
+    const allowedMethods = reallyAllowedMethods(req, params);
+    if (allowedMethods.length && !program.epfirst) {
+      next();
+    } else {
+      fetch(endpoint + req.path, {
+        method: req.method,
+        headers: {
+          'Accept': req.headers.accept,
+          'Authorization': req.headers.authorization,
+          'Content-type': req.headers['content-type']
+        },
+      }).then(function(response) {
+        const contentType = response.headers.get('content-type');
+        if (contentType) {
+          res.set('Content-type', response.headers.get('content-type'));
+        }
+        return response.text();
+      }).then(function(text) {
+        res.send(text);
+      }).catch(function(response) {
+        console.log(response);
+        res.sendStatus(404);
+      });
+    }
+  } else {
+    next();
+  }
+});
+
+const port = program.port;
+module.exports.port = port;
+
+const apiDataPath = program.path;
+module.exports.apiDataPath = apiDataPath;
+
+app.all('/*', function(req, res) {
+  const params = getQueryString(req);
 
   console.info('HTTP ' + req.method + ' ' + req.path + ' ' + params);
 
   if (req.method === 'OPTIONS') {
-    const methods = allowedMethods.filter(function(method) {
-      const filePath = path.join(module.exports.apiDataPath, req.path + params + '.' + method + '.json');
-      if(fs.existsSync(filePath)) {
-        return method;
-      }
-    });
+    const methods = reallyAllowedMethods(req, params);
     if(methods.length) {
       res.setHeader('Access-Control-Allow-Methods', methods.join(', '));
       res.send(methods);
