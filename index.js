21d2682e7c3ed7ab6329d4e2766bdc43774eb538
Paolo Ferretti <paolo.ferretti@contactlab.com>
2016-11-14 10:22:38 +0100

    Version bump to 1.3.0

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const DEFAULT_PORT = 8081;
 const DEFAULT_PATH = path.join(__dirname, 'data');
 
 program
-  .version('1.1.1')
+  .version('1.3.0')
   .description("'Yet Another Rest API Stubber'.split(' ').reverse().map(item => item[0].toLowerCase()).join('')")
   .option('--port <port>', 'The port to listen to (default: 8081)', DEFAULT_PORT)
   .option('--path <password>', 'The path for stubbed data (default ./data)', DEFAULT_PATH)
