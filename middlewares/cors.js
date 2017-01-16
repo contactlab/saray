0674a9c1d6ef44633aec8c27832f408b9f55a982
Paolo Ferretti <paolo@paoloferretti.net>
2017-01-16 10:59:31 +0100

    Refs #32 - Fixed bug for custome headers in CORS requests + integration tests

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,9 +1,20 @@
 const utils = require('../utils');
 
+function updateCORSAllowedHeaders (req, allowedHeaders) {
+  const requestHeaders = req.get('Access-Control-Request-Headers');
+  if (requestHeaders) {
+    allowedHeaders += ', ' + requestHeaders;
+  }
+  return allowedHeaders;
+}
+
 function cors(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', utils.allowedMethods.join(', '));
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  const allowedHeaders = 'Origin, X-Requested-With, Content-Type, Accept, Authorization';
+  res.setHeader('Access-Control-Allow-Headers', updateCORSAllowedHeaders(req, allowedHeaders));
+
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
