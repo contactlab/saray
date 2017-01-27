72c5717968fc0d258ad3cbb74a99dbda9754366c
Paolo Ferretti <paolo@paoloferretti.net>
2017-01-27 12:12:34 +0100

    Version bump to 1.6.0

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const DEFAULT_LOG_PATH = path.join(__dirname, 'saray.log');
 const DEFAULT_ROOT_PATH = '';
 
 program
-  .version('1.5.3')
-  .description("'Yet Another Rest API Stubber'.split(' ').reverse().map(item => item[0].toLowerCase()).join('')")
+  .version('1.6.0')
+  .description('\'Yet Another Rest API Stubber\'.split(\' \').reverse().map(item => item[0].toLowerCase()).join(\'\')')
   .option('--port <port>', 'The port to listen to (default: 8081)', DEFAULT_PORT)
   .option('--path <password>', 'The path for stubbed data (default ./data)', DEFAULT_PATH)
   .option('--endpoint <endpoint>', 'The endpoint (default null)', null)
